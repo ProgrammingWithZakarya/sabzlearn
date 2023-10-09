80253e936bdcfb275888e49860505e99280be230
Zakarya HasanZadeh <hasanzadahzakarya@gmail.com>
2023-10-10 01:21:33 +0330

    updated footer and stylings

--- frontend/src/Components/FooterItem/FooterItem.jsx
+++ frontend/src/Components/FooterItem/FooterItem.jsx
@@ -5,13 +5,12 @@ import './FooterItem.css'
 export default function FooterItem({ title, children }) {
   return (
     <div className="col-4">
-    <div className="footer-widgets__item">
-      <span className="footer-widgets__title">
-        {title}
-      </span>
-
-      {children}
+      <div className="footer-widgets__item">
+        <span className="footer-widgets__title">
+          {title}
+        </span>
+        {children}
+      </div>
     </div>
-  </div>
   )
 }
