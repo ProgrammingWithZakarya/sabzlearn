1adea25d0a1823cecfa8441cdfef19c5da54c37c
Zakarya HasanZadeh <hasanzadahzakarya@gmail.com>
2023-10-10 01:27:45 +0330

    added index page p-admin and styling

---
+++ frontend/src/pages/AdminPanel/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../../Components/AdminPanel/Sidebar/Sidebar";
+import Topbar from "../../Components/AdminPanel/Topbar/Topbar";
+import './Index.css'
+export default function index() {
+  return (
+    <>
+      <div id="content">
+        <Sidebar />
+      </div>
+      <div id="home" class="col-10">
+        <Topbar />
+        <div class="container-fluid" id="home-content">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+}
