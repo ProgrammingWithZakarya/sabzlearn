8c3dfcda90e5105a3f3e425cf99c53d55aa717aa
Zakarya HasanZadeh <hasanzadahzakarya@gmail.com>
2023-10-10 01:42:21 +0330

    added domPurify to project and  fetching datas from dataBase andupdate styling

--- frontend/src/pages/ArticleInfo/ArticleInfo.js
+++ frontend/src/pages/ArticleInfo/ArticleInfo.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Topbar from "./../../Components/Topbar/Topbar";
 import Navbar from "./../../Components/Navbar/Navbar";
 import Footer from "./../../Components/Footer/Footer";
 import Breadcrumb from "./../../Components/Breadcrumb/Breadcrumb";
+import domPurify from 'dompurify'
 
 import "./ArticleInfo.css";
 import CommentsTextArea from "../../Components/CommentsTextArea/CommentsTextArea";
-
+import { useParams } from "react-router-dom";
 export default function ArticleInfo() {
+  const [article  , setArticle] = useState({})
+  useEffect(() => {
+    getAllDatas()
+  } ,[])
+  const {articleName} = useParams()
+  function getAllDatas () {
+    fetch(`http://localhost:4000/v1/article/${articleName}`)
+  }
   return (
     <>
       <Topbar />
       <Navbar />
-
       <Breadcrumb
         links={[
           { id: 1, title: "خانه", to: "" },
@@ -143,8 +151,8 @@ export default function ArticleInfo() {
                   alt="Article_Image"
                   className="article__seconadary-banner"
                 />
-                <div className="article-section">
-                  <h2 className="article-section__title">
+                <div className="article-section" dangerouslySetInnerHTML={{__html :domPurify.sanitize(article.body)}}>
+                  {/* <h2 className="article-section__title">
                     معرفی بهترین سایت ‌های آموزش جاوا اسکریپت:
                   </h2>
                   <p className="paragraph article-section__text">
@@ -162,46 +170,8 @@ export default function ArticleInfo() {
                     src="/images/blog/4.png"
                     alt="article body img"
                     className="article-section__img"
-                  />
+                  /> */}
                 </div>
-                <div className="article-section">
-                  <h2 className="article-section__title">
-                    معرفی بهترین سایت ‌های آموزش جاوا اسکریپت:
-                  </h2>
-                  <p className="paragraph article-section__text">
-                    توجه داشته باشید که تمام وب سایت‌هایی که به عنوان بهترین
-                    سایت آموزش جاوا اسکریپت در ادامه معرفی می‌کنیم، بین‌المللی
-                    هستند و منابع موجود در آن‌ها به زبان انگلیسی است. در نتیجه
-                    شما باید یا تسلط متوسط و حداقلی به زبان انگلیسی داشته باشید
-                    و یا اینکه با استفاده از گوگل ترنسلیت منابع موجود را ترجمه
-                    کرده و از آن‌ها استفاده کنید. به همین دلیل در انتهای محتوا
-                    به شما خواهیم گفت که راه آسان دیگری برای یادگیری زبان جاوا
-                    اسکریپت وجود دارد که شما بتوانید به واسطه آن به صورت رایگان
-                    و به زبان فارسی این زبان را یاد بگیرید.
-                  </p>
-                </div>
-                <div className="article-section">
-                  <h2 className="article-section__title">
-                    معرفی بهترین سایت ‌های آموزش جاوا اسکریپت:
-                  </h2>
-                  <p className="paragraph article-section__text">
-                    توجه داشته باشید که تمام وب سایت‌هایی که به عنوان بهترین
-                    سایت آموزش جاوا اسکریپت در ادامه معرفی می‌کنیم، بین‌المللی
-                    هستند و منابع موجود در آن‌ها به زبان انگلیسی است. در نتیجه
-                    شما باید یا تسلط متوسط و حداقلی به زبان انگلیسی داشته باشید
-                    و یا اینکه با استفاده از گوگل ترنسلیت منابع موجود را ترجمه
-                    کرده و از آن‌ها استفاده کنید. به همین دلیل در انتهای محتوا
-                    به شما خواهیم گفت که راه آسان دیگری برای یادگیری زبان جاوا
-                    اسکریپت وجود دارد که شما بتوانید به واسطه آن به صورت رایگان
-                    و به زبان فارسی این زبان را یاد بگیرید.
-                  </p>
-                  <img
-                    src="/images/blog/3.jpg"
-                    alt="article body img"
-                    className="article-section__img"
-                  />
-                </div>
-
                 <div className="article-social-media">
                   <span className="article-social-media__text">اشتراک گذاری :</span>
                   <a href="##" className="article-social-media__link">
